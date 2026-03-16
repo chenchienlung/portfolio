7c8cbcc538c4e60ff25504e0189dd646d94c57f4
Chris <chris27805850@gmail.com>
2026-03-16 17:20:52 +0800

    自動部署Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
+  base: '/portfolio',
   plugins: [tailwindcss(), vue(), vueDevTools()],
   resolve: {
     alias: {
