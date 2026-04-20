775d14c4c94a951a47efecb94cfda8d2f89a83d7
Chris <chris27805850@gmail.com>
2026-04-20 15:31:39 +0800

    新增404頁面深色模式

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -2,10 +2,10 @@
   <main
     class="h-[calc(100dvh-136px)] md:h-[calc(100dvh-40px)] flex flex-col items-center justify-center gap-6 text-center"
   >
-    <p class="text-8xl font-bold text-gray-600">404</p>
-    <h1 class="text-2xl text-gray-800">找不到這個頁面！</h1>
+    <p class="text-8xl font-bold text-gray-600 dark:text-gray-300">＊ 404 Not Found ＊</p>
+    <h1 class="text-2xl text-gray-800 dark:text-gray-200">＊ 哇！找不到這個頁面！ ＊</h1>
     <img :src="src" alt="404" class="w-50 h-50 rounded-xl" fetchpriority="high" />
-    <p class="text-gray-600">您輸入的網址不存在，或是已經被移除了。</p>
+    <p class="text-lg text-gray-600 dark:text-gray-300">您輸入的網址不存在，或是已經被移除了。</p>
     <RouterLink
       to="/"
       class="px-6 py-3 text-sm text-white bg-sky-800 hover:bg-sky-700 rounded-full transition-all duration-200"
