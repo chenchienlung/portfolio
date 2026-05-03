e15bccc6471f0f75daa90688b1157a35a6621a0b
Chris <chris27805850@gmail.com>
2026-05-03 19:18:01 +0800

    修改Navbar 在深色模式的背景色

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -4,19 +4,19 @@
     :class="isIOSSafari ? 'bottom-[4px]' : 'bottom-5'"
   >
     <div
-      class="w-fit mx-auto flex flex-row justify-center items-center gap-2 p-1 bg-gray-200/30 dark:bg-white/5 backdrop-blur-sm inset-shadow-sm rounded-full border border-black/5 dark:border-white/10"
+      class="w-fit mx-auto flex flex-row justify-center items-center gap-2 p-1 bg-gray-200/30 dark:bg-black/40 backdrop-blur-sm inset-shadow-sm rounded-full border border-black/5 dark:border-white/10"
     >
       <ul
-        class="w-[134px] flex flex-row justify-center p-0.5 bg-gray-200/45 dark:bg-white/10 backdrop-blur-sm inset-shadow-sm rounded-full border border-black/5 dark:border-white/10"
+        class="w-[134px] flex flex-row justify-center p-0.5 bg-gray-200/45 dark:bg-black/20 backdrop-blur-sm inset-shadow-sm rounded-full border border-black/5 dark:border-white/10"
       >
         <li v-for="item in navItems" :key="item.to">
           <RouterLink
             :to="item.to"
             :class="[
-              'h-10 flex flex-row items-center justify-center rounded-full transition-all duration-300 ease-out text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white hover:bg-black/5 dark:hover:bg-white/10',
+              'h-10 flex flex-row items-center justify-center rounded-full transition-all duration-300 ease-out text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white hover:bg-black/35 dark:hover:bg-white/10',
               item.match(route.path) ? 'w-22' : 'w-10',
             ]"
-            active-class="!bg-gray-50 dark:!bg-white/10 shadow-sm inset-shadow-black/10 dark:inset-shadow-white inset-shadow-sm/50 dark:inset-shadow-sm/20 !text-gray-900 dark:!text-white hover:!bg-white dark:hover:!bg-gray-600/90"
+            active-class="!bg-gray-50 dark:!bg-white/10 shadow-sm inset-shadow-black/10 dark:inset-shadow-white/60 inset-shadow-sm/50 dark:inset-shadow-sm/20 !text-gray-900 dark:!text-white"
           >
             <font-awesome-icon :icon="item.icon" class="w-10 h-10 mb-px shrink-0" />
             <span
