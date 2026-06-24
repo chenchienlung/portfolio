98fcf2d40ba547aca07e904a446662aa6c783d9f
Chris <chris27805850@gmail.com>
2026-06-24 15:23:06 +0800

    footer 細節調整

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,14 +1,10 @@
 <template>
-  <footer class="w-full h-fit text-black/80 dark:text-white/80">
-    <div class="mx-auto flex flex-row justify-center items-center gap-2 pb-20 md:pb-4">
+  <footer class="max-w-6xl mx-auto mt-5 h-fit text-black/80 dark:text-white/80">
+    <div
+      class="mx-5 p-5 flex flex-col lg:flex-row justify-between items-center mb-24 md:mb-5 bg-white dark:bg-white/5 shadow-xs/12 border border-black/15 dark:border-white/15 rounded-3xl">
       <p>Portfolio by Chris Chen © 2026</p>
-      <a
-        href="https://github.com/chenchienlung"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="hover:text-sky-600 transition-all duration-200"
-        title="我的GitHub"
-      >
+      <a href="https://github.com/chenchienlung" target="_blank" rel="noopener noreferrer"
+        class="hover:text-sky-600 transition-all duration-200" title="我的GitHub">
         <font-awesome-icon icon="fa-brands fa-github" class="mr-0.5" />GITHUB
       </a>
     </div>
